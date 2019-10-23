57a2e090fcf8623e126ccb285bf9ad987daeef0a
Iwanami0306 <turmeric.naminami.1073@gmail.com>
2019-10-23 14:40:17 +0900

    json形式で生データを表示できる

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,2 +1,14 @@
 class PapersController < ApplicationController
+    
+    def index
+        @papers = Paper.all
+        render :json => papers
+    end
+    def all
+        @papers = Paper.all
+        @keywords = Keyword.all
+        @authors = Author.all
+
+        render :json => {:papers => @papers,:keywords => @keywords, :authors=> @authors}
+    end
 end
