6f44958dbbdf6b351575088734bffc4cb0591f9a
Iwanami0306 <turmeric.naminami.1073@gmail.com>
2019-10-25 17:44:35 +0900

    coreの設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@
 
      resource '*',
        headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+       methods: [:get, :post, :put, :patch, :delete, :options, :head, :all, :create]
        expose: ['Per-Page', 'Total', 'Link']
    end
  end
