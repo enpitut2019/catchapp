bf80dd6980bfd632d2ef17db6e997274db16010d
Iwanami0306 <turmeric.naminami.1073@gmail.com>
2019-10-25 16:38:06 +0900

    finish API test

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -4,11 +4,28 @@ class PapersController < ApplicationController
         @papers = Paper.all
         render :json => papers
     end
+
+
+    def create
+        @paper = Paper.new(paper_params)
+        if @paper.save
+            render json: @paper, status: :created, location: @user
+        else
+            render json: @paper.errors, status: :unprocessable_entity
+        end
+    end
+
     def all
         @papers = Paper.all
         @keywords = Keyword.all
         @authors = Author.all
 
         render :json => {:papers => @papers,:keywords => @keywords, :authors=> @authors}
+    end
+
+    private
+
+    def paper_params
+        params.permit(:abstract,:title,:publishedAt,:url)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "papers/all"
   get "keywords/index"
   get "authors/index"
+  post "papers/create"
 end
