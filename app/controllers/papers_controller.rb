774ed21dd1dc5e660652f893698cae7964f3b969
Aika-Murakami <aika.murakami@yourmystar.jp>
2019-10-30 13:57:50 +0900

    json形式の変更

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,14 +1,11 @@
 class PapersController < ApplicationController
-    
     def index
         @papers = Paper.all
         render :json => papers
     end
     def all
         @papers = Paper.all
-        @keywords = Keyword.all
-        @authors = Author.all
 
-        render :json => {:papers => @papers,:keywords => @keywords, :authors=> @authors}
+        render :json => {:papers => @papers.to_json(:include => [:authors, :keywords])}
     end
 end
