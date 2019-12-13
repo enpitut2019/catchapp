3ad5869a5c1d78995d0f8c3fadbdb8eb21a2b635
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-12-13 15:03:22 +0900

    add: Google翻訳を追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 require 'rexml/document'
 require 'net/http'
 require 'uri'
-# require "google/cloud/translate"
 
 class SearchController < ApplicationController
     def get_xml
@@ -23,6 +22,21 @@ class SearchController < ApplicationController
     end
 
     private
+    # Google Translateで翻訳する関数
+    def translate(content)
+      url = URI.parse('https://www.googleapis.com/language/translate/v2')
+      params = {
+        q: content,
+        target: "ja", # 翻訳したい言語
+        source: "en", # 翻訳する言語の種類
+        key: ENV["GOOGLE_CLOUD_API_KEY"]
+      }
+      url.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(url)
+      json = res.body
+
+      return JSON.parse(json)["data"]["translations"].first["translatedText"]
+    end
 
     # arXiv APIから取得したXMLを解析してPaperモデルの配列を返す関数
     def parseXML(xmlString)
@@ -33,8 +47,10 @@ class SearchController < ApplicationController
         doc = REXML::Document.new(xmlString)
         doc.elements.each('//entry') do |e|
             title = e.elements['title'] ? e.elements['title'].text : ''
+            title_ja = translate(title)
             arxiv_id = e.elements['id'] ? e.elements['id'].text : ''
             abstract = e.elements['summary'] ? e.elements['summary'].text : ''
+            abstract_ja = translate(abstract)
             url = e.elements['link[@type="text/html"]'] ? e.elements['link[@type="text/html"]'].attributes['href'] : ''
             url_pdf = e.elements['link[@type="application/pdf"]'] ? e.elements['link[@type="application/pdf"]'].attributes['href'] : ''
             published_at = e.elements['published'] ? e.elements['published'].text : ''
@@ -50,7 +66,7 @@ class SearchController < ApplicationController
             paper = Paper.find_by(:aixiv_id => arxiv_id)
             if paper.nil? then
               ## PaperがDBに存在しなければ保存してpaper変数を更新
-              paper = Paper.create!(title: title, url: url, pdf_url: url_pdf, journal: journal, abstract: abstract, aixiv_id: arxiv_id, published_at: DateTime.parse(published_at))
+              paper = Paper.create!(title: title, title_ja: title_ja, url: url, pdf_url: url_pdf, journal: journal, abstract: abstract, abstract_ja: abstract_ja, aixiv_id: arxiv_id, published_at: DateTime.parse(published_at))
               ## Authorsの保存
               author_names.map do |author_name|
                 paper.authors.create!(name: author_name)
