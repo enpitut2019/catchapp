cb55a7a2d17334126d0494a08c6673ab3f2cccbe
Mikihiro SUDA <su.glasses.923@gmail.com>
2020-01-20 16:48:51 +0900

    change: ルーティングを整理する

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get "papers/index" # 非推奨
+  get "papers/all" # 非推奨
+  get "papers", to: "papers#all"
   get "paper/:id", to: "papers#show"
   get "keywords/index"
   get "authors/index"
   post "papers/create"
-  post "papers/upload"
-  post "search/get_xml"
+  post "papers/upload" # 非推奨
+  post "figures/upload", to: "papers#upload"
+  post "search/get_xml" # 非推奨
+  get "papers/search", to: "search#get_xml"
   get "translate", to: "translate#get"
   post "translate", to: "translate#set"
   post "papers/get_figure"
